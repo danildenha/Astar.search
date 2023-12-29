bbf8a72d311b74e27687c73e12a2df4616937711
danildenha <danildenha@gmail.com>
2023-12-28 21:23:31 -0600

    Update source.py

--- Run Files/source.py
+++ Run Files/source.py
@@ -6,4 +6,16 @@ WIDTH = 1000
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Search by danildena")
 
+#Color values for the nodes
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (233, 30, 233)
+ORANGE = (255, 135 ,0)
+GREY = (140, 140, 140)
+TURQUOISE = (48, 213, 200)
+
 
