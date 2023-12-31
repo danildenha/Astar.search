ec7e194f5b2b5cb3c4bb9e842232f3ab93c5dbd2
danildenha <danildenha@gmail.com>
2023-12-31 13:28:59 -0600

    Update source.py

--- Run Files/source.py
+++ Run Files/source.py
@@ -136,11 +136,16 @@ def algorithm(draw, grid, start, end):
                 came_from[neighbor] = current
                 g_score[current] = temp_g_score
                 f_score[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())
+                #if node is not yet in priority queue
                 if neighbor not in open_set_hash:
                      count += 1
                      open_set.put(f_score[neighbor], count, neighbor)
                      open_set_hash.add(neighbor)
-                     neighbor.make_closed()
+                     neighbor.make_open()
+
+        draw()
+        if current != start:
+            current.make_closed()
 
 
 
