012af1636f91491a4f7e057c578c77e30c5ab8b2
danildenha <danildenha@gmail.com>
2023-12-31 13:27:16 -0600

    Update source.py

--- Run Files/source.py
+++ Run Files/source.py
@@ -136,6 +136,11 @@ def algorithm(draw, grid, start, end):
                 came_from[neighbor] = current
                 g_score[current] = temp_g_score
                 f_score[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                     count += 1
+                     open_set.put(f_score[neighbor], count, neighbor)
+                     open_set_hash.add(neighbor)
+                     neighbor.make_closed()
 
 
 
