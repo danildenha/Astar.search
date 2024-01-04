ffbaf5cdb02b803b9352b761e5522d2f076b784e
danildenha <danildenha@gmail.com>
2024-01-04 17:16:37 -0600

    Update source.py

--- Run Files/source.py
+++ Run Files/source.py
@@ -241,10 +241,7 @@ def main(win, width):
 
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
                     
-                if event.key == pygame.K_c:
-                    start = None
-                    end = None
-                    grid = make_grid(ROWS, width)
+                
 
 
     pygame.quit()
